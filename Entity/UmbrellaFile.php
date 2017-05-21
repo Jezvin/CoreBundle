e3f34c7483e41b99d8b7b75e50cede017ca251b1
acantepie <adrien.cantepie@gmail.com>
2017-05-21 22:50:11 +0200

    Manage file (not finished)

---
+++ Entity/UmbrellaFile.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: acantepie
+ * Date: 21/05/17
+ * Time: 22:08
+ */
+
+namespace Umbrella\CoreBundle\Entity;
+
+use Umbrella\CoreBundle\Core\BaseEntity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class UmbrellaFile
+ * @package Umbrella\CoreBundle\Entity
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="umbrella_file")
+ */
+class UmbrellaFile extends BaseEntity
+{
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    public $name;
+
+    /**
+     * @var integer
+     * @ORM\Column(type="smallint", nullable=false)
+     */
+    public $size;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", nullable=false)
+     */
+    public $md5;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", nullable=false)
+     */
+    public $mimeType;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", nullable=false)
+     */
+    public $fileName;
+
+}
