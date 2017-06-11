78ef05dce949093e3cc32f9b95cec065fd0d25dd
acantepie <adrien.cantepie@gmail.com>
2017-06-11 14:27:23 +0200

    ChoiceType extension

--- Form/Extension/ChoiceTypeExtension.php
+++ Form/Extension/ChoiceTypeExtension.php
@@ -5,11 +5,11 @@
  * Date: 29/05/17
  * Time: 21:52.
  */
-
 namespace Umbrella\CoreBundle\Form\Extension;
 
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -17,10 +17,22 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class ChoiceTypeExtension extends AbstractTypeExtension
 {
+    /**
+     * @param OptionsResolver $resolver
+     */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefault('choice_label', function ($value, $key, $index) {
-            return empty($key) ? $key : 'form.choice.'.strtolower($key);
+        $resolver->setDefault('translate_choice', true);
+        $resolver->setAllowedTypes('translate_choice', [ 'bool' ]);
+
+        $resolver->setNormalizer('choice_label', function (Options $options, $value) {
+            if ($value === null && $options['translate_choice']) {
+                return function ($value, $key, $index) {
+                    return empty($key) ? $key : 'form.choice.' . strtolower($key);
+                };
+            } else {
+                return $value;
+            }
         });
     }
 
