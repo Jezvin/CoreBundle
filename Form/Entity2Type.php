08739d3c2d389eb97720422d27c04dbb62e9e35a
acantepie <adrien.cantepie@gmail.com>
2017-06-14 22:27:49 +0200

    Fix form

--- Form/Entity2Type.php
+++ Form/Entity2Type.php
@@ -9,26 +9,12 @@
 namespace Umbrella\CoreBundle\Form;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Class Entity2Type.
  */
-class Entity2Type extends AbstractType
+class Entity2Type extends Choice2Type
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults(array(
-            'attr' => array(
-                'class' => 'js-select2',
-                'style' => 'width: 100%',
-            ),
-        ));
-    }
 
     /**
      * {@inheritdoc}
